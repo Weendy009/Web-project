0772f220928a239275548f7b40ab1fb349b04dad
Matvey Postnikov <ostnikovnorav@gmail.com>
2023-04-17 09:24:31 +0300

    first commit

---
+++ Weendy/src/main/java/com/project/Weendys/WeendysApplication.java
@@ -0,0 +1,13 @@
+package com.project.Weendys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeendysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeendysApplication.class, args);
+	}
+
+}
---
+++ Weendy/src/test/java/com/project/Weendys/WeendysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Weendys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeendysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
